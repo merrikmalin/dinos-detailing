dd76cd42e0e50010a5e58f089b25b03427e5ba07
Merrik Malin <merrik.malin7@gmail.com>
2025-01-01 23:14:16 -0500

    Created full website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterOutlet
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dinos-detailing';
+    title = 'dinos-detailing';
+    servicesUrl = '/assets/documents/dinos-detailing-services.pdf';
+    email = 'dinosdetailss@gmail.com';
+    phoneNumber = '(740) 296-8807';
+    phoneNumberRaw = '+17402968807'; // for tel: links
+
+    instagramUrl = 'https://www.instagram.com/dinosdetailing_/';
+    youtubeUrl = 'https://youtube.com/@dinosdetailingg?si=pY6nqhg77knbE7UF';
+    tiktokUrl = 'https://www.tiktok.com/@dinos.detailing6';
 }
